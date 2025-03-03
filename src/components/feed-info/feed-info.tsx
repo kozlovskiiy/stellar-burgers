6c98bee04c2733c2504f65ffb3e2c56e4fe15d0a
Stanislav Kozlovskiy <stan.kozlowscky@yandex.ru>
2025-03-03 16:16:05 +0300

    Количество заказов

--- src/components/feed-info/feed-info.tsx
+++ src/components/feed-info/feed-info.tsx
@@ -1,7 +1,40 @@
-import { FC } from 'react';
+// import { FC, useEffect } from 'react';
+
+// import { TOrder } from '@utils-types';
+// import { FeedInfoUI } from '../ui/feed-info';
+// import { useDispatch, useSelector } from '../../services/store';
+// import { fetchOrders, selectOrders } from '../../slices/feedSlice';
+
+// const getOrders = (orders: TOrder[], status: string): number[] =>
+//   orders
+//     .filter((item) => item.status === status)
+//     .map((item) => item.number)
+//     .slice(0, 20);
+
+// export const FeedInfo: FC = () => {
+//   /** TODO: взять переменные из стора */
+//   const orders: TOrder[] = [];
+//   const feed = {};
+
+//   const readyOrders = getOrders(orders, 'done');
+
+//   const pendingOrders = getOrders(orders, 'pending');
+
+//   return (
+//     <FeedInfoUI
+//       readyOrders={readyOrders}
+//       pendingOrders={pendingOrders}
+//       feed={feed}
+//     />
+//   );
+// };
+
+import { FC, useEffect } from 'react';
 
 import { TOrder } from '@utils-types';
 import { FeedInfoUI } from '../ui/feed-info';
+import { useDispatch, useSelector } from '../../services/store';
+import { fetchOrders, selectOrders } from '../../slices/feedSlice';
 
 const getOrders = (orders: TOrder[], status: string): number[] =>
   orders
@@ -11,8 +44,12 @@ const getOrders = (orders: TOrder[], status: string): number[] =>
 
 export const FeedInfo: FC = () => {
   /** TODO: взять переменные из стора */
-  const orders: TOrder[] = [];
-  const feed = {};
+  const { orders = [], total, totalToday } = useSelector(selectOrders) || {};
+
+  const feed = {
+    total,
+    totalToday
+  };
 
   const readyOrders = getOrders(orders, 'done');
 
@@ -26,3 +63,7 @@ export const FeedInfo: FC = () => {
     />
   );
 };
+
+// const orders: TOrder[] = useAppSelector(selectOrders);
+// const total = useAppSelector(selectTotalOrders);
+// const totalToday = useAppSelector(selectTodayOrders);
--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,10 +1,8 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
-import { TOrder } from '@utils-types';
 import { FC } from 'react';
-import { useSelector } from 'react-redux';
 import { selectOrders } from '../../slices/feedSlice';
-import { useDispatch } from '../../services/store';
+import { useDispatch, useSelector } from '../../services/store';
 import { useEffect } from 'react';
 import { fetchOrders } from '../../slices/feedSlice';
 
@@ -15,8 +13,6 @@ export const Feed: FC = () => {
   useEffect(() => {
     dispatch(fetchOrders());
   }, [dispatch]);
-  /** TODO: взять переменную из стора */
-  // const orders: TOrder[] = [];
 
   if (!orders || !orders.orders.length) {
     return <Preloader />;
