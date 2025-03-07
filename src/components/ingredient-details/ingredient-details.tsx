0bcd082346c73f9102c85f6991a843683bf1e084
Stanislav Kozlovskiy <stan.kozlowscky@yandex.ru>
2025-03-07 12:37:02 +0300

    UseSelector from store

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector } from '../../services/store';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
-import { selectIngredients } from '../../slices/ingredientsSlice'; // Предположительно, селектор списка ингредиентов
+import { selectIngredients } from '../../slices/ingredientsSlice';
 
 export const IngredientDetails: FC = () => {
   const { id } = useParams();
