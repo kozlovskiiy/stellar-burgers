eb3b010c655f0b0aae09b6b92bb56d4b501eb620
Stanislav Kozlovskiy <stan.kozlowscky@yandex.ru>
2025-03-07 12:41:28 +0300

    Отображается пустая страница есть пользователь не сделал заказы

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -8,14 +8,9 @@ import { TOrder } from '@utils-types';
 export const ProfileOrders: FC = () => {
   const dispatch = useDispatch();
   const orders: TOrder[] = useSelector(userOrdersSelector);
-  console.log(orders);
   useEffect(() => {
     dispatch(getOrdersThunk());
   }, []);
-
-  if (orders.length === 0) {
-    return <Preloader />;
-  }
 
   return <ProfileOrdersUI orders={orders} />;
 };
